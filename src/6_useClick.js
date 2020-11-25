839710012b07b267cfcded583eab8387766d1d85
parksohyeon <dso753@elinmedia.com>
2020-11-25 14:46:59 +0900

    useClick 예제

---
+++ src/6_useClick.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect, useRef } from "react";
+import ReactDom from "react-dom";
+
+const useClick = (onClick) => {
+    if (typeof onClick !== "function") {
+        return;
+    }
+
+    const element = useRef();
+
+    useEffect(() => {
+        if (element.current) {
+            element.current.addEventListener("click", onClick);
+        }
+
+        return () => {
+            if (element.current) {
+                element.current.removeEventListener("click", onClick);
+            }
+        };
+    }, []);
+
+    return element;
+};
+
+export default function App() {
+    const sayHello = () => console.log("say hello");
+    const title = useClick(sayHello);
+
+    return (
+        <div className="App">
+            <h1 ref={title}>Hi</h1>
+        </div>
+    );
+}
