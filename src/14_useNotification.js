31c2294c55d8c510fa6ca3cb79c511fb738a01e4
parksohyeon <dso753@elinmedia.com>
2020-11-28 01:55:55 +0900

    useNotification 예제

---
+++ src/14_useNotification.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect, useRef } from "react";
+import ReactDom from "react-dom";
+
+const useNotification = (title, options) => {
+    if (!("Notification" in window)) {
+        return;
+    }
+
+    const fireNotif = () => {
+        if (Notification.permission !== "granted") {
+            Notification.requestPermission().then((permission) => {
+                if (permission === "granted") {
+                    new Notification(title, options);
+                } else {
+                    return;
+                }
+            });
+        } else {
+            new Notification(title, options);
+        }
+    };
+
+    return fireNotif;
+};
+
+export default function App() {
+    const triggerNotif = useNotification("Can I steal your Kimchi?", {
+        body: "I love kimchi dont you"
+    });
+    return (
+        <div className="App" style={{ height: "1000vh" }}>
+            <button onClick={triggerNotif}>Hi</button>
+        </div>
+    );
+}
